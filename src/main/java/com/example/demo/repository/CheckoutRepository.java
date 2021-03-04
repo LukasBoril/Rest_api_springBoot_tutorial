ca71fe1c31fbbcda9b1c9e18980947c3599d8b55
bacn <mbachmann@trdi.io>
2021-03-04 09:58:55 +0100

    remove tag annotation

--- src/main/java/com/example/demo/repository/CheckoutRepository.java
+++ src/main/java/com/example/demo/repository/CheckoutRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 /**
  * Repository class for Checkout
  */
-@Tag(description = "Checkout from Cart for a customer", name="Checkout")
 @RepositoryRestResource(exported = true)
 public interface CheckoutRepository extends CrudRepository<Checkout, Long> {
 }
