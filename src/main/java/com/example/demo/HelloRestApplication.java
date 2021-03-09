82b5c2d1fc25dbf5368717ed515d37e6819ea7e8
bacn <mbachmann@trdi.io>
2021-03-09 18:04:00 +0100

    new profiles mysql and h2

--- src/main/java/com/example/demo/HelloRestApplication.java
+++ src/main/java/com/example/demo/HelloRestApplication.java
@@ -28,13 +28,16 @@ public class HelloRestApplication {
     @PostConstruct
     public void afterInit() {
         boolean hasDevProfile = Arrays.asList(env.getActiveProfiles()).contains("dev");
+        boolean hasH2Database = Arrays.asList(env.getActiveProfiles()).contains("h2");
         String applicationName = env.getProperty("spring.application.name");
         String openApiInfo="";
         String h2ConsoleInfo="";
         if (hasDevProfile) {
-            openApiInfo= "http://localhost:8080/v3/api-docs\n" +
+            openApiInfo = "http://localhost:8080/v3/api-docs\n" +
                     "http://localhost:8080/v3/api-docs.yaml -> yaml file is downloaded -> https://editor.swagger.io/\n" +
                     "http://localhost:8080/swagger-ui.html \n";
+        }
+        if (hasH2Database) {
             h2ConsoleInfo= "http://localhost:8080/h2-console  " + "" +
                     "-> mit Generic H2 (Embedded), org.h2.Driver, jdbc:h2:mem:testdb und sa \n";
         }
@@ -43,6 +46,7 @@ public class HelloRestApplication {
                 h2ConsoleInfo + "\n" +
                 "Active Profiles: " + Arrays.toString(env.getActiveProfiles()) + "\n\n");
     }
+
     @Bean
     @Profile("dev")
     public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
